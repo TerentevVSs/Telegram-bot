ac8cdd6d6dc3409c00ea2edbf72531c145ea3bfe
Sashashen <71884699+Sashashen@users.noreply.github.com>
2020-12-03 20:56:23 +0300

    Update image.py

--- image.py
+++ image.py
@@ -1,25 +1,25 @@
 import cv2
 import numpy as np
 def determinant(namelist, height, width, j, n):
-    # функция выводит вес(на пиксель), и координаты центра масс изменения между двумя изображениями в списке под номерами j, n
-    M = 0
+    # функция выводит геометрическую длину изменения(на пиксель),
+    # и координаты центра изменения между двумя изображениями(под номерами j, n) в списке(namelist)
+    Len = 0
     y = 0
     x = 0
     for i in range(height):
         for k in range(width):
+            #rj, gj, bj - значения пикселя с координатами i,k изображения под номером j
             rj, gj, bj = namelist[j][i][k]
             rn, gn, bn = namelist[n][i][k]
-            m = ((rj - rn) ** 2 + (gj - gn) ** 2 + (bj - bn) ** 2) ** 0.5
-            M = M + m
-            x = x + k * m
-            y = y + i * m
-    y = y / M
-    x = x / M
-    M = M/(height*width)
-    return [M, x, y]
+            delta = ((rj - rn) ** 2 + (gj - gn) ** 2 + (bj - bn) ** 2) ** 0.5
+            Len = Len + delta
+            x = x + k * delta
+            y = y + i * delta
+    y = y / Len
+    x = x / Len
+    Len = Len/(height*width)
+    return [Len, x, y]
 def rgb(namephoto, namelist, i):
-    # функция записывает на i место в список изображение ввиде значения rgb
+    # функция записывает на i место в список(namelist) изображение в попиксельном виде [r,g,b]
     img = cv2.imread(namephoto)
     namelist.insert(i, np.asarray(img))
-
-
