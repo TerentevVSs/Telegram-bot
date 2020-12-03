133d388573187b8bf62ce1e00228bf7dea7f49d0
Sashashen <71884699+Sashashen@users.noreply.github.com>
2020-12-03 20:36:54 +0300

    Update image.py

--- image.py
+++ image.py
@@ -3,8 +3,8 @@ import numpy as np
 def determinant(namelist, height, width, j, n):
     # функция выводит вес(на пиксель), и координаты центра масс изменения между двумя изображениями в списке под номерами j, n
     M = 0
-    y = height / 2
-    x = width / 2
+    y = 0
+    x = 0
     for i in range(height):
         for k in range(width):
             rj, gj, bj = namelist[j][i][k]
