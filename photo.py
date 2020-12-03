ee36aa1574ac920713018a0e00e10e713d8cad01
Sashashen <71884699+Sashashen@users.noreply.github.com>
2020-12-03 20:59:28 +0300

    Update photo.py

--- photo.py
+++ photo.py
@@ -4,8 +4,5 @@ picrez = [0]*2
 image.rgb("c2.png", picrez, 0)
 image.rgb("c3.png", picrez, 1)
 height, width = picrez[0].shape[:2]
-M = 0
-y = height/2
-x = width/2
 M, x, y = image.determinant(picrez, height, width, 0, 1)
 print(M,x,y)
