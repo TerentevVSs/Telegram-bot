cba0c96f908a763ae51f762cebd24c558b5045fc
TerentevVSs <71519431+TerentevVSs@users.noreply.github.com>
2020-12-04 18:45:00 +0300

    Поделил все значения на 255

--- module_image.py
+++ module_image.py
@@ -1,21 +1,27 @@
 import numpy as np
+
+
 def delta(namelist):
     '''Args: namelist
     Return: функция считает сумму квадратов разностей r,g,b по пикселям двух изображений'''
     C = np.add(namelist[0], -namelist[1])
     C=C.T
-    C[0][C[0]<=100]=0
+    C[0][C[0]<=0.5]=0
     С = np.dot(C, C.T)
     C = np.sum(C)
     return C
-def rgb(name_photo, namelist, i, height, width):
-    ''' Args: name_photo, namelist, i, height, width
-    name_photo: название изображения
-    namelist: список в который будет занесено изображение в
-    попиксельном виде
-    i: номер места на который изображение будет занесено в список
-    height: размер изображение по вертикали
-    width: размер изображение по горизонтали
-    Returns: функция преобразует изображение(img) в вектор и записывает в массив(namelist) на i место'''
-    array = np.array(name_photo, dtype='uint8').reshape((1, (height)*(width)*3)).T
-    namelist.insert(i, array)
+
+
+def rgb(name_photo, height, width):
+    """
+    функция преобразует изображение(img) в вектор
+    Args:
+        name_photo: название изображения
+        height: размер изображение по вертикали
+        width: размер изображение по горизонтали
+    Returns:
+        array: функция преобразует изображение(img) в вектор
+    """
+    array = np.array(name_photo, dtype='uint8').reshape(
+        (1, (height) * (width) * 3)).T/255
+    return array
