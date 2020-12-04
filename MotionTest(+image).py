f53c3e5d4d8e87ce591f93efb01be47e69498021
TerentevVSs <71519431+TerentevVSs@users.noreply.github.com>
2020-12-04 18:45:49 +0300

    Теперь сравнивает на основе первых 25 кадров

--- MotionTest(+image).py
+++ MotionTest(+image).py
@@ -2,36 +2,55 @@ import cv2
 import numpy as np
 import module_image as image
 import matplotlib.pyplot as plt
+
 count = 0
 path_of_the_center = []
-couple = [0] * 3
-cap = cv2.VideoCapture('test.mp4')
+couple = [0] * 2
+cap = cv2.VideoCapture('test3.mp4')
 
 # Проверяем, открывается ли видео
 if (cap.isOpened() == False):
     print("Error opening video stream or file")
-x=[]
-y=[]
+x = []
+y = []
+sum = 0
 # Открываем видео для чтения до окончания процесса
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         count += 1
-        if count >= 2:
-            image.rgb(frame, couple, 1, height, width)
+        if count > 21:
+            couple[1] = image.rgb(frame, height, width)
             Delta = image.delta(couple)
             print(Delta)
             x.append(count)
             y.append(Delta)
-        else:
+        elif count == 21:
+            average = sum / 20
+            couple[0] = average
+            couple[1] = image.rgb(frame, height, width)
+            Delta = image.delta(couple)
+            x.append(count)
+            y.append(Delta)
+        elif count==1:
             height, width = frame.shape[:2]
-            image.rgb(frame, couple, count, height, width)
+            couple[1] = image.rgb(frame, height, width)
             x0 = int(width / 2)
             y0 = int(height / 2)
+            Delta = image.delta(couple)
             x.append(count)
+            sum += couple[1]
+        else:
+            height, width = frame.shape[:2]
+            couple[1] = image.rgb(frame, height, width)
+            x0 = int(width / 2)
+            y0 = int(height / 2)
+            Delta = image.delta(couple)
+            x.append(count)
+            y.append(Delta)
+            sum += couple[1]
         frame_to_show = cv2.circle(frame, (x0, y0), 1, 255, 1)
         cv2.imshow('Frame', frame_to_show)
-        print(count)
         # Press Q on keyboard to  exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
