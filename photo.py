962e9e10b7bdb1a383c20bcebd84f6e417a7bbff
TerentevVSs <71519431+TerentevVSs@users.noreply.github.com>
2020-12-13 13:27:24 +0300

    Удалена устаревшая версия

--- photo.py
+++
@@ -1,8 +0,0 @@
-# пример работы
-import image
-picrez = [0]*2
-image.rgb("c2.png", picrez, 0)
-image.rgb("c3.png", picrez, 1)
-height, width = picrez[0].shape[:2]
-M, x, y = image.determinant(picrez, height, width, 0, 1)
-print(M,x,y)
