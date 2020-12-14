c9af4a7112b662ab30988484787fe5eb286add64
TerentevVSs <71519431+TerentevVSs@users.noreply.github.com>
2020-12-14 21:40:39 +0300

    Адаптировано под нужды программы

--- Neural_Network.py
+++ Neural_Network.py
@@ -16,13 +16,5 @@ def check_image(image, parameters=parameters):
     Returns:
         prediction: предсказанное значение
     """
-    size_x = 160
-    size_y = 90
-    size = (size_x, size_y)
-    image = Image.open(image)
-    image = image.resize(size)
-    array = np.array(image, dtype='uint8')
-    array = array.reshape((1, size_x * size_y * 3)).T / 255
-    data_test = array
-    prediction = predict(data_test, parameters)
+    prediction = predict(image, parameters)
     return prediction
