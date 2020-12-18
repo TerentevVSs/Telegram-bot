f614505b1423bd8eeb08ba02154714928edc656a
protopopova <protopopova.md@phystech.edu>
2020-12-18 13:58:44 +0700

    ПРОВЕРЯТЬ ТОЛЬКО ПО-ОДНОМУ!!!!!1

--- MotionTest.py
+++ MotionTest.py
@@ -3,6 +3,7 @@ import module_image as image
 import Neural_Network
 import numpy as np
 import telebot
+
 from telebot import types
 
 bot = telebot. TeleBot("1427672157:AAH_K0LbWsIuNyUR3vQC9tEvxxrkHlqVEuY")
@@ -38,11 +39,7 @@ def choose_mode(message):
     global it_is_day
     if message.text == 'День':
         it_is_day = True  # Булева функция дня и ночи
-    elif message.text == 'Ночь':
-        it_is_day = False  # Булева функция дня и ночи
-    bot.register_next_step_handler(message, lets_work)  # следующий шаг – функция  lets_work
-
-def lets_work(message):
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAKGY1_bde_LilNGDnjcXIBFL93uqlwEAAJiFgAC6VUFGKq9bs1J0e3SHgQ')
     count = 0
     couple = [0] * 2
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -88,8 +85,8 @@ def lets_work(message):
                 couple[1] = image.rgb(frame, m, n)
                 Delta = image.delta(couple)
                 delta_duo = image.delta([couple[1], old_frame])
-                if Delta > medium_delta*(params["delta_high"] +
-                                         medium_duo/delta_duo) \
+                if Delta > medium_delta * (params["delta_high"] +
+                                            medium_duo / delta_duo) \
                         and delta_duo > medium_duo * params["duo_high"]:
                     it_moves = True
                     if count % 3 == 0:
@@ -97,14 +94,15 @@ def lets_work(message):
                         neural_network_check = np.array(
                             neural_network_check, dtype='uint8')
                         neural_network_check = cv2.resize(
-                            neural_network_check, dsize=(160, 90),
+                               neural_network_check, dsize=(160, 90),
                             interpolation=cv2.INTER_CUBIC)
                         neural_network_check = neural_network_check.reshape(
-                            160*90*3, 1) / 255
+                            160 * 90 * 3, 1) / 255
                         worry = Neural_Network.check_image(neural_network_check)
                         if worry == 1:
                             # Тут вывод в бота
                             bot.send_message(message.chat.id, 'ЗАФИКСИРОВАН ЧЕЛОВЕК')
+                            print('men')
                 else:
                     # проверка было ли движение
                     if it_moves:
@@ -115,20 +113,20 @@ def lets_work(message):
                     else:
                         # движение не было иди прошло, проверка для
                         # добавление кадр в усреднение
-                        if Delta < params["delta_low"] * medium_delta\
+                        if Delta < params["delta_low"] * medium_delta \
                                 and delta_duo < medium_duo * params["duo_low"]:
                             # кадр подходит для усреднения
                             static_set[len_static] = static_set[len_static] \
-                                         * len_static \
-                                        - static_set[position_static_set][0]
+                                                        * len_static \
+                                                        - static_set[position_static_set][0]
                             static_set[position_static_set] = [couple[1],
-                                                               Delta]
+                                                                Delta]
                             static_set[len_static] = (static_set[len_static] +
-                                        static_set[position_static_set][0]) /\
-                                                     len_static
+                                                        static_set[position_static_set][0]) / \
+                                                        len_static
                             couple[0] = static_set[len_static]
                             position_static_set = (position_static_set + 1) \
-                                                  % len_static
+                                                      % len_static
             elif count == len_static + 1:
                 static_set[count - 2] = [couple[1], Delta]
                 # вычислениек medium_delta
@@ -143,7 +141,7 @@ def lets_work(message):
                 Delta = image.delta(couple)
                 # вычисление medium_duo
                 medium_duo += image.delta([static_set[count - 2][0],
-                                           static_set[count - 3][0]])
+                                            static_set[count - 3][0]])
                 medium_duo = medium_duo / len_static
             elif count == 1:
                 height, width = frame.shape[:2]
@@ -155,7 +153,7 @@ def lets_work(message):
                 # подсчёт
                 if count > 2:
                     medium_duo += image.delta([static_set[count - 2][0],
-                                               static_set[count-3][0]])
+                                                static_set[count - 3][0]])
                 sum_delta = sum_delta + Delta
                 # обновление среднего кадра
                 static_set[len_static] = static_set[len_static] * (count - 2)
@@ -169,9 +167,14 @@ def lets_work(message):
                 break
         else:
             break
-    # Выход из видео, когда процесс окончен
+        # Выход из видео, когда процесс окончен
     cap.release()
     cv2.destroyAllWindows()
+    #bot.register_next_step_handler(message, lets_work)  # следующий шаг – функция  lets_work
+
+
+
+#def lets_work(message):
 
 @bot.message_handler(content_types = ['sticker'])
 def sticker_id(message):
